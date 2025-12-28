975fffc51513bbf15eb169e069fc15163e768e63
Stephie-Grafika <164401918+Stephie-Grafika@users.noreply.github.com>
2025-12-28 20:57:12 +0100

    HTML Grundstruktur
    Struktur für Rechner im HTML
    Anlage JS-Datei mit Kommentaren (geplante Funktionen)

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Variablen für Ausgabe von Rechenweg und Ergebnis
+let eingegebenerRechenweg = "0";
+let berechnetesErgebnis = false;
+
+// Rechenweg anzeigen
+function Hinzufuegen(zeichen){
+    document.getElementById('rechenweg').value += zeichen;
+}
+
+// Ergebnis berechnen & ausgeben
+
+/* Werte aus Buttons in Zahlen umwandeln mit Number(), damit wir rechnen können, 
+    weil Inhalt als String mit "X" übergeben werden */
+    let eingabe1 = Number(eingabe1.value);
+
+
+//ergebnis.innerHTML = userEingabe;
