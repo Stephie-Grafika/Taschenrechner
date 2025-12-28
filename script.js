de6b95adbdc35b3ccf1434aa2dbd71e829164685
Stephie-Grafika <164401918+Stephie-Grafika@users.noreply.github.com>
2025-12-28 22:55:50 +0100

    JS Ergebnisberechnung
    * Ergebnis aus String mit eval
    * komplette Eingabe löschen
    * replace für Berechnung

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
 'use strict';
 
-// Variablen für Ausgabe von Rechenweg und Ergebnis
-let eingegebenerRechenweg = "0";
-let berechnetesErgebnis = false;
+// Konstanten in HTML-Elementen, weil diese in mehreren Funktionen verwendet werden und somit kürzer angesprochen werden können
+const ausgabeRechenweg = document.getElementById('rechenweg');
+const ausgabeErgebnis = document.getElementById('ergebnis');
 
 // Rechenweg anzeigen
 function hinzufuegen(zeichen){
-    document.getElementById('rechenweg').value += zeichen;
+    ausgabeRechenweg.value += zeichen;
 }
 
 // Ergebnis berechnen & ausgeben
 
-/* Werte aus Buttons in Zahlen umwandeln mit Number(), damit wir rechnen können, 
-    weil Inhalt als String mit "X" übergeben werden */
-    let eingabe1 = Number(eingabe1.value);
+function berechnen(){
+    let rechenwegEingabe = ausgabeRechenweg.value;
+
+    let rechenwegIntern = rechenwegEingabe.replace(/x/g, '*')
+                        .replace(/÷/g, '/')
+                        .replace(/,/g, '.')
+                        .replace(/%/g, '/100');
+
+    try {
+        // eval() berechnet einen String als mathematische Formel
+        let ergebnis = eval(rechenwegIntern);
+        ausgabeErgebnis.value = '= ' + ergebnis.toString().replace('.', ',');
+    } catch (error){
+        ausgabeErgebnis.value = "Kann nicht berechnet werden wegen felerhafter Eingabe."
+    }
+}
+
+// keine Eingabe vor =
 
 // letzte Eingabe löschen
+
 // komplette Einagbe löschen
+function allesLoeschen() {
+    ausgabeRechenweg.value = "";
+    ausgabeErgebnis.value = "";
+}
+
 // Vorzeichen
 
 //ergebnis.innerHTML = userEingabe;
